2ac71f3c69e0faed36f30d9531d7df6185f6ae72
notkyleleo <61864540+notkyleleo@users.noreply.github.com>
2024-01-28 01:27:11 +0800

    Added NPC Animation Frameworks

--- Assets/Scripts/NPCAnimHandler.cs
+++ Assets/Scripts/NPCAnimHandler.cs
@@ -14,21 +14,21 @@ public class NPCAnimHandler : MonoBehaviour
 
     private void IdleNPC(Animator animator)
     {
-        
+        animator.SetTrigger("idle");
     }
 
     private void ShockNPC(Animator animator)
     {
-
+        animator.SetTrigger("shock");
     }
 
     private void RunningNPC(Animator animator)
     {
-
+        animator.SetTrigger("running");
     }
     private void LaughNPC(Animator animator)
     {
-        
+        animator.SetTrigger("laugh");
     }
 
     private void OnDestroy()
