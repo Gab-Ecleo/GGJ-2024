96539d587c457f692d0c6db9bf5862422599baf0
notkyleleo <61864540+notkyleleo@users.noreply.github.com>
2024-01-28 03:49:39 +0800

    Added human randomizer and spawner

--- Assets/Scripts/EscapeRoute.cs
+++ Assets/Scripts/EscapeRoute.cs
@@ -7,9 +7,17 @@ public class EscapeRoute : MonoBehaviour
 {
     public Transform[] escapeRoute;
 
+    [SerializeField] private GameObject[] humans;
+
     private void Awake()
     {
         escapeRoute = GetComponentsInChildren<Transform>().Skip(1).ToArray();
+        
+        for(int n = 0; n < escapeRoute.Length; n++)
+        {
+            var human = Instantiate(humans[Random.Range(0, humans.Length)]);
+            human.transform.position = escapeRoute[n].transform.position;
+        }
     }
 
     private void Start()
