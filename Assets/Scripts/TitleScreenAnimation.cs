171c86069d9442d03d7e1de97022ebf4b8d332af
Pistajhoe <jhoedinm@gmail.com>
2024-02-02 23:17:40 +0800

    Title Screen Subtitle
    - "press any key to continue" subtitle added
    - subtitle fade animation added
    - open and close menu function
    - main menu panel color adjusted

--- Assets/Scripts/TitleScreenAnimation.cs
+++ Assets/Scripts/TitleScreenAnimation.cs
@@ -1,20 +1,72 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScreenAnimation : MonoBehaviour
 {
+    [SerializeField] private float animationDelay;
+
+    #region Title Screen Switch
     private Animator animator;
+    private bool menuIsOpen;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-
+        Cursor.lockState = CursorLockMode.None;
+        menuIsOpen = false;
     }
 
     private void Update()
     {
-        if (!Input.anyKey) return;
-        animator.SetTrigger("OpenMenu");
+        if (menuIsOpen) CloseMenu();
+        else OpenMenu();
     }
+
+    private void OpenMenu()
+    {
+        if (!Input.anyKeyDown) return;
+        TriggerAnimation("OpenMenu");
+    }
+
+    private void CloseMenu()
+    {
+        if (!Input.GetKeyDown(KeyCode.Escape)) return;
+        TriggerAnimation("CloseMenu");
+    }
+
+    private void TriggerAnimation(string animation)
+    {
+        animator.SetTrigger(animation);
+        menuIsOpen = !menuIsOpen;
+    }
+    #endregion
+
+    #region Transition to Play the Game
+    public void Play()
+    {
+        StartCoroutine(GameTransition());
+    }
+
+    private IEnumerator GameTransition()
+    {
+        animator.SetTrigger("PlayGame");
+        yield return new WaitForSeconds(animationDelay);
+        SceneManager.LoadScene(1);
+    }
+    #endregion
+
+    #region Transition to Quit Application
+    public void Exit()
+    {
+        StartCoroutine(ExitTransition());
+    }
+
+    private IEnumerator ExitTransition()
+    {
+        animator.SetTrigger("PlayGame");
+        yield return new WaitForSeconds(animationDelay);
+        Application.Quit();
+    }
+    #endregion
 }
